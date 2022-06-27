5d65d78765c1cca46135cd63ccbeed5590ca698c
Pushkar <pushkarnagarsenker@gmail.com>
2022-06-27 14:44:31 +0530

    added a class for service layer

---
+++ src/main/java/com/numinolabs/PNCitizenAPI/Services/CitizenService.java
@@ -0,0 +1,11 @@
+package com.numinolabs.PNCitizenAPI.Services;
+
+import com.numinolabs.PNCitizenAPI.Repository.CitizenRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public abstract class CitizenService implements CitizenRepository {
+
+
+
+}
