a9182f6447b51efa762645d325370f9e3b53a849
Pushkar <pushkarnagarsenker@gmail.com>
2022-06-27 14:40:39 +0530

    Added Exception handler class to the project

---
+++ src/main/java/com/numinolabs/PNCitizenAPI/Exception/AppExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.numinolabs.PNCitizenAPI.Exception;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Object> HandleException(Exception ex, WebRequest request){
+        System.out.println("getLocalizedMessage :" + ex.getLocalizedMessage());
+        return new ResponseEntity<>(ex.getLocalizedMessage(), new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = NoSuchElementException.class)
+    public ResponseEntity<Object> HandleNullPointerException(Exception ex, WebRequest request){
+        System.out.println("getLocalizedMessage :" + ex.getLocalizedMessage());
+        return new ResponseEntity<>(ex.getLocalizedMessage(), new HttpHeaders(),HttpStatus.NOT_FOUND);
+    }
+}
