985afaa460c90f7f97584f0ba1a6c868797bf541
Pushkar <pushkarnagarsenker@gmail.com>
2022-06-27 14:43:54 +0530

    cleaned the Code for all request mapping types

--- src/main/java/com/numinolabs/PNCitizenAPI/Controller/ApiController.java
+++ src/main/java/com/numinolabs/PNCitizenAPI/Controller/ApiController.java
@@ -1,20 +1,24 @@
 package com.numinolabs.PNCitizenAPI.Controller;
 
 import com.numinolabs.PNCitizenAPI.Model.Citizen;
-import com.numinolabs.PNCitizenAPI.Repo.CitizenRepo;
+import com.numinolabs.PNCitizenAPI.Repository.CitizenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "users")
 public class ApiController {
+
     @Autowired
-    private CitizenRepo citizenRepo;
+    private CitizenRepository citizenRepo;
 
     @GetMapping(path = "")
     public List<Citizen> getCitizen(){
@@ -23,44 +27,35 @@ public class ApiController {
 
     @GetMapping(path = "/{id}",produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<Citizen> getUser(@PathVariable long id){
-        Citizen citizen = citizenRepo.findById(id).get();
-
+        Citizen citizen =  citizenRepo.findById(id).get();
         return new ResponseEntity<Citizen>(citizen, HttpStatus.OK);
     }
 
-//    @PostMapping
-//    public String createCitizen(@RequestBody Citizen citizen){
-//        citizenRepo.save(citizen);
-//        return "Saved";
-//    }
+    @PostMapping(path = "")
+    public ResponseEntity<Object> createCitizens(@RequestBody Citizen citizen){
+        Citizen savedCitizen = citizenRepo.save(citizen);
 
-    @PostMapping
-    public String createCitizens(@RequestBody List<Citizen> citizen){
-        for (Citizen c: citizen
-             ) {
-            citizenRepo.save(c);
-        }
-        return "saved list of users...";
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedCitizen.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @PutMapping(path = "/{id}")
-    public String updateCitizen(@PathVariable(value = "id") Long id,@RequestBody Citizen citizen){
-        Citizen UpdatedCitizen = citizenRepo.findById(id).get();
-        UpdatedCitizen.setFullName(citizen.getFullName());
-        UpdatedCitizen.setAadharId(citizen.getAadharId());
-        UpdatedCitizen.setDateOfBirth(citizen.getDateOfBirth());
-        UpdatedCitizen.setGender(citizen.getGender());
-        UpdatedCitizen.setPincode(citizen.getPincode());
-        UpdatedCitizen.setState(citizen.getState());
-        citizenRepo.save(UpdatedCitizen);
-        return "Updated..";
+    public ResponseEntity<Object> updateCitizen(@PathVariable(value = "id") Long id,@RequestBody Citizen citizen){
+
+        Optional<Citizen> studentOptional = citizenRepo.findById(id);
+        if (!studentOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        citizen.setId(id);
+        citizenRepo.save(citizen);
+        return ResponseEntity.noContent().build();
+
     }
 
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Void> deleteCitizen(@PathVariable Long id){
-        Citizen citizen = citizenRepo.findById(id).get();
-        citizenRepo.delete(citizen);
-        return ResponseEntity.noContent().build();
-//        return "Deleted user with id: "+ id.toString();
+    public void deleteCitizen(@PathVariable(value = "id") Long id){
+        citizenRepo.deleteById(id);
     }
 }
