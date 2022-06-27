a25d6aad9adae8c3abf152875ff551f2f2110bda
Pushkar <pushkarnagarsenker@gmail.com>
2022-06-27 14:43:02 +0530

    renamed CitizenRepo.java to CitizenRepository.java

--- src/main/java/com/numinolabs/PNCitizenAPI/Repo/CitizenRepo.java
+++
@@ -1,8 +0,0 @@
-package com.numinolabs.PNCitizenAPI.Repo;
-
-import com.numinolabs.PNCitizenAPI.Model.Citizen;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CitizenRepo extends JpaRepository<Citizen,Long> {
-
-}
